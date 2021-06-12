b5b717874f07b07e94bf2fb1786fb4522d718d50
Vignesh <iamvigneshr6@gmail.com>
2021-06-12 19:33:58 +0530

    file db removed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into user (id,username,password,email,dob) values
- (1,'vigneshun','$2y$10$IL2jI8tPQbpyM20HPFWy1.reNW0H0d7ICO3LKMj/XVamvV.YNcYH6','vignesh@examplecom',parsedatetime('1994-06-22','yyyy-MM-dd'));
+ (1,'vigneshun','$2y$10$IL2jI8tPQbpyM20HPFWy1.reNW0H0d7ICO3LKMj/XVamvV.YNcYH6','vignesh@examplecom',parsedatetime('1990-09-01','yyyy-MM-dd'));
 
  insert into user_roles(user_id,role) values
  (1,1);
